614975663fad4050d1386dfe5a06664d56db423d
popeye0618 <89083254+popeye0618@users.noreply.github.com>
2024-01-22 06:54:56 +0000

    bin

---
+++ bin-search/16401.py
@@ -0,0 +1,31 @@
+#과자 나눠주기(실버 2)
+#내 풀이
+#과자의 길이를 합칠수가 없다.
+#필요한 조건은 과자의 길이가 조카의 수보다 작을 경우 불가능
+#과자를 자르는 동작을 이분탐색으로 구현하고
+#자른 후에 과자가 몇 개인지, 조카의 수보다 많거나 같으면 저장 후 left 증가
+
+n, k = map(int, input().split())
+snacks = list(map(int, input().split()))
+
+result = 0
+if sum(snacks) < n:
+  print(0)
+else:
+  snacks.sort()
+  left = 1
+  right = snacks[-1]
+  while left <= right:
+    mid = (left + right) // 2
+    cnt = 0
+  
+    for i in range(len(snacks)):
+      cnt += snacks[i] // mid
+  
+    if cnt >= n:
+      result = mid
+      left = mid + 1
+    else:
+      right = mid - 1
+
+  print(result)
---
+++ bin-search/1822.py
@@ -0,0 +1,46 @@
+#차집합(실버 4)
+#내 풀이
+#이것도 그냥 간단한 이분탐색
+
+# n, m = map(int, input().split())
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+
+# tmp = set()
+# a.sort()
+
+# for x in b:
+#   left = 0
+#   right = n - 1
+#   while left <= right:
+#     mid = (left + right) // 2
+
+#     if x == a[mid]:
+#       tmp.add(a[mid])
+#       break
+#     elif x > a[mid]:
+#       left = mid + 1
+#     else:
+#       right = mid - 1
+
+# answer = [x for x in a if x not in tmp]
+# if len(answer) == 0:
+#   print(0)
+# else:
+#   print(len(answer))
+#   answer.sort()
+#   print(' '.join(map(str, answer)))
+
+#파이썬이 사기인 이유
+n, m = map(int, input().split())
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+
+answer = a - b
+
+if len(answer) == 0:
+  print(0)
+else:
+  print(len(answer))
+  for x in sorted(answer):
+    print(x, end=' ')
